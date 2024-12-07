872dcb36d92c3a713ccacccce8d32adbb834f022
melanysosa <sosamelanynoelia@gmail.com>
2024-12-07 14:43:51 -0300

    about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CardImagesComponent } from '../../shared/components/card-images/card-images.component';
+import { CardServicesComponent } from '../../shared/components/card-services/card-services.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CardImagesComponent, CardServicesComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  imageUrl = '/assets/images/team-5.jpg';
+  cardsAbout = [
+    {
+      id: 1,
+      title: 'Diseño Residencial',
+      imageUrl: '/assets/images/team-3.jpg',
+      hoverText:
+        'Creamos hogares que combinan confort, funcionalidad y estética, adaptándonos a tu estilo de vida y necesidades.',
+    },
+    {
+      id: 2,
+      title: 'Diseño Comercial',
+      imageUrl: '/assets/images/team-4.jpg',
+      hoverText:
+        'Creamos soluciones funcionales para negocios y empresas con diseños personalizados.',
+    },
+    {
+      id: 3,
+      title: 'Consultoría de estilo',
+      imageUrl: '/assets/images/team-2.jpg',
+      hoverText:
+        'Te ayudamos a definir el mejor estilo para tus espacios con soluciones únicas.',
+    },
+  ];
 }
---
+++ src/app/shared/components/card-images/card-images.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-images',
+  imports: [],
+  templateUrl: './card-images.component.html',
+  styleUrl: './card-images.component.css',
+})
+export class CardImagesComponent {
+  @Input() title: string = '';
+  @Input() imageUrl: string = '';
+  @Input() hoverText: string = '';
+}
